5db70d6fa0a0fcc1748749784b23fc3f7d65f246
Sadymix <korzeniewskiadamkonrad@gmail.com>
2022-02-16 15:21:24 +0100

    implemented PodamUtility for test enviroment makepojo methodto create pojo

---
+++ src/test/java/com/example/shopapp/utility/PodamUtility.java
@@ -0,0 +1,18 @@
+package com.example.shopapp.utility;
+
+import lombok.Data;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+@Data
+public class PodamUtility {
+
+    private static PodamFactory FACTORY = new PodamFactoryImpl();
+    private PodamUtility() {
+
+    }
+
+    public static <T> T makePojo(Class<T> tClass){
+        return FACTORY.manufacturePojo(tClass);
+    }
+}
