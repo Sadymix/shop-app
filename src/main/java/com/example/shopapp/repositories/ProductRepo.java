83cb9a4f6afb2dab5b5c41852872a11acec7c4c6
Sadymix <korzeniewskiadamkonrad@gmail.com>
2022-03-19 23:18:18 +0100

    modified id from UUID to Long

--- src/main/java/com/example/shopapp/repositories/ProductRepo.java
+++ src/main/java/com/example/shopapp/repositories/ProductRepo.java
@@ -4,8 +4,6 @@ import com.example.shopapp.models.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface ProductRepo extends JpaRepository<Product, UUID> {
+public interface ProductRepo extends JpaRepository<Product, Long> {
 }
