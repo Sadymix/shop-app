eb7868b0a4f80bbf945d153f7c6cdfa0eab22461
Sadymix <korzeniewskiadamkonrad@gmail.com>
2022-03-19 23:26:32 +0100

    implemented UserValidator with validateSingleUser method that checks if principal of authentication is instance of User class

---
+++ src/main/java/com/example/shopapp/validators/UserValidator.java
@@ -0,0 +1,26 @@
+package com.example.shopapp.validators;
+
+import com.example.shopapp.exceptions.IllegalUserException;
+import com.example.shopapp.models.entity.Role;
+import com.example.shopapp.models.entity.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+@AllArgsConstructor
+public class UserValidator {
+
+    public void validateSingleUser(Long id, Authentication authentication) {
+        if(authentication.getPrincipal() instanceof User user) {
+            var hasAdmin = user.getRoles().stream()
+                    .map(Role::getName)
+                    .anyMatch("ADMIN"::equals);
+            if(!(Objects.equals(user.getId(),id) || hasAdmin)) {
+                throw new IllegalUserException("The token doesn't have the permission to access single user.");
+            }
+        }
+    }
+}
