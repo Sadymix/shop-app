128edb03e4446c2f46e067558471544cdfd1d393
Sadymix <korzeniewskiadamkonrad@gmail.com>
2022-03-19 23:10:19 +0100

    modified sql scripts

--- src/main/resources/database.2022-02-14/02-product-data.sql
+++ src/main/resources/database.2022-02-14/02-product-data.sql
@@ -1,3 +1,4 @@
 --liquibase formatted sql
---changeset kkorzeniewski:insert-products-into-products-table
-insert into products(id, name, price, color, type) values (UUID_TO_BIN(UUID()), 'shirt', 59.99, 'black', 'CLOTHES');
+--changeset kkorzeniewski:2
+INSERT INTO products(name, price, color, type)
+VALUE("shirt", 59.99, "black", "CLOTHES");
